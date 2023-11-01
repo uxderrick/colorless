81ff0c460f9a147bf5cb35a63dc706bf71bdb45d
Derrick Tsorme <49356701+uxderrick@users.noreply.github.com>
2023-11-01 02:00:17 +0000

    Now storing an array of the lightness values

--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Flex, Text } from "@radix-ui/themes";
 
 const Result = ({ colorData, hsl, colorInput, lightness }) => {
+  // Initialize an empty array for shades and tints
+  const shadesAndTints = [];
+
+  // Define the getTintsAndShades function to calculate shades and tints
+  const getTintsAndShades = (lightness) => {
+    let uniqueTintsAndShades = [];
+
+    // Calculate tints
+    for (
+      let i = lightness;
+      i <= 100 && uniqueTintsAndShades.length < 11;
+      i += 10
+    ) {
+      const value = i;
+      uniqueTintsAndShades.push(value);
+    }
+
+    // Calculate shades
+    for (
+      let i = lightness;
+      i >= 0 && uniqueTintsAndShades.length < 11;
+      i -= 10
+    ) {
+      const value = i;
+      uniqueTintsAndShades.push(value);
+    }
+
+    // Remove duplicates
+    uniqueTintsAndShades = [...new Set(uniqueTintsAndShades)];
+
+    // Update the shadesAndTints array
+    shadesAndTints.length = 0;
+    shadesAndTints.push(...uniqueTintsAndShades);
+  };
+
+  useEffect(() => {
+    console.log("Lightness", lightness);
+    getTintsAndShades(lightness);
+
+    // Sort the shadesAndTints array
+    shadesAndTints.sort((a, b) => a - b);
+
+    // Log the shadesAndTints array
+    console.log("Shades and Tints", shadesAndTints);
+  }, [lightness]);
+
   return (
     <Flex
       direction="row"
