ad8954d94f046871a363e3725bdb6777e62110d3
Derrick Tsorme <49356701+uxderrick@users.noreply.github.com>
2023-11-02 16:25:14 +0000

    Fixed the error screen

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -7,8 +7,8 @@ const Error = () => {
       {/* Empty State */}
       <Flex direction="column" justify="center" align="center" gap="3" py="6">
         <img src="src/assets/warning sign.png" alt="logo" height="80px" />
-        <Text align="center" className="grey-text">
-          Enter a correct color code
+        <Text align="center" className="grey-text width">
+          Enter a correct color code. eg., #000000, #ffffff, #ff0000, #00ff00,
         </Text>
       </Flex>
     </>
