fd38b2aa0e0bae690a16f576a40d8b80b46ca509
Derrick Tsorme <49356701+uxderrick@users.noreply.github.com>
2023-11-08 13:00:42 +0000

    lighter tints of the same color

--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -15,7 +15,7 @@ const Result = ({ colorData, hsl, colorInput, lightness, hue, saturation }) => {
     for (
       let i = lightness;
       i <= 100 && uniqueTintsAndShades.length < 31;
-      i += 5
+      i += 3
     ) {
       const value = i;
       uniqueTintsAndShades.push(value);
