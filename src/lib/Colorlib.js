e3a639517b56106c2742b1aa19c0374be1a1ffd3
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-11 17:08:55 +0000

    convert hex to hsl locally

---
+++ src/lib/Colorlib.js
@@ -0,0 +1,25 @@
+import { hexToHsva, hsvaToHslString, hslStringToHsva, hsvaToHex } from '@uiw/color-convert'
+
+/**
+ * convert hex to hsl
+ * @param {string} hex 
+ * @return {{ h:number, s:number, l:number, value:string}}
+ */
+export function hexToHsl(hex) {
+    const hsva = hexToHsva(hex)
+    const hslStr = hsvaToHslString(hsva)
+
+    // hslStr is in format hsl(342, 100%, 77%)
+    const match = hslStr.match(/^hsl\(([\d.]+), ([\d.]+)%, ([\d.]+)%\)$/);
+    if (!match) {
+        throw new Error(`Invalid HSL string: ${hslStr}`);
+    }
+
+    return {
+        h: parseFloat(match[1]),
+        s: parseFloat(match[2]),
+        l: parseFloat(match[3]),
+        value: hslStr
+    };
+}
+
